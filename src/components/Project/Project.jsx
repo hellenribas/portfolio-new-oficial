2d83892ee3ba76b2bd554f44ed747ee0c74a963f
hellenribas <tchwtrabalho@gmail.com>
2022-05-28 10:52:02 -0300

    adicionando projetos

--- src/components/Project/Project.jsx
+++ src/components/Project/Project.jsx
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
-import ImageLayout from '../images/layout.png';
+// import ImageLayout from '../images/layout.png';
 import TodoList from '../images/todo-list-of.png';
 import PixelArt from '../images/pixels-art-of.png';
 import './Project.css';
 import ColorGuess from '../images/color-guess-of.png';
 import { IoIosArrowDroprightCircle } from 'react-icons/io';
 import Trybewallet from '../images/trybewallet.png';
+import Trybetunes from '../images/trybetunes.png';
+import Solar from '../images/system-solar.png';
+import Recipes from '../images/mobile-app-recipes.png';
+import Mistery from '../images/mistery-letter.png';
+import {IoIosAddCircleOutline} from 'react-icons/io';
 
 export default class Project extends Component {
   constructor() {
@@ -54,12 +59,25 @@ export default class Project extends Component {
         </div>
         <main className='project-search'>
           <div className='carousel' id='carousel'>
+            <div className='img-carousel'>
+            <a href='.' target='_blank' rel="noreferrer">
+              <img src={Recipes} alt='project6' className='wall-6' />
+              <p>APP RECEITAS</p>
+              </a>
+            </div>
           <div className='img-carousel'>
               <a href='https://github.com/hellenribas/trybeWallet' target='_blank' rel="noreferrer">
               <img src={Trybewallet} alt='project4' />
               <p>TRYBEWALLET</p>
               </a>
             </div>
+            <div className='img-carousel'>
+            <a href='https://github.com/hellenribas/trybetunes' target='_blank' rel="noreferrer">
+              <img src={Trybetunes}
+                alt='project5' className='wall-5' />
+              <p>TRYBETUNES</p>
+              </a>
+              </div>
             <div className='img-carousel'>
             <a href='https://github.com/hellenribas/todo-list' rel="noreferrer" target='_blank'>
               <img src={TodoList} alt='project3'
@@ -82,29 +100,21 @@ export default class Project extends Component {
               </a>
             </div>
             <div className='img-carousel'>
-              <img src={ImageLayout}
-                alt='project5' className='wall-5' />
-              <p>Img5</p>
+            <a href='https://github.com/hellenribas/solar-system' target='_blank' rel="noreferrer">
+              <img src={Solar} alt='project6' className='wall-6' />
+              <p>SISTEMA SOLAR</p>
+              </a>
             </div>
             <div className='img-carousel'>
-              <img src={ImageLayout} alt='project6' className='wall-6' />
-              <p>Img6</p>
+            <a href='https://github.com/hellenribas/mistery-letter' target='_blank' rel="noreferrer">
+              <img src={Mistery} alt='project6' className='wall-6' />
+              <p>Mistery Letter</p>
+              </a>
             </div>
             <div className='img-carousel'>
-              <img src={ImageLayout} alt='project6' className='wall-6' />
-              <p>Img7</p>
-            </div>
-            <div className='img-carousel'>
-              <img src={ImageLayout} alt='project6' className='wall-6' />
-              <p>Img8</p>
-            </div>
-            <div className='img-carousel'>
-              <img src={ImageLayout} alt='project6' className='wall-6' />
-              <p>Img9</p>
-            </div>
-            <div className='img-carousel'>
-              <img src={ImageLayout} alt='project6' className='wall-6' />
-              <p>Img10</p>
+            <a href='https://github.com/hellenribas?tab=repositories' target='_blank' rel="noreferrer">
+              <IoIosAddCircleOutline className='add' />
+              </a>
             </div>
           </div>
           <div className='button-container'>
